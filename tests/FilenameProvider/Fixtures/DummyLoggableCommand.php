19306bc4868bde6b776067c1d966a916d862213c
HypeMC <hypemc@gmail.com>
2022-06-06 00:21:16 +0200

    Add AsCommand attribute

--- tests/FilenameProvider/Fixtures/DummyLoggableCommand.php
+++ tests/FilenameProvider/Fixtures/DummyLoggableCommand.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Bizkit\LoggableCommandBundle\Tests\FilenameProvider\Fixtures;
 
 use Bizkit\LoggableCommandBundle\Command\LoggableCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+#[AsCommand(name: self::DEFAULT_NAME)]
 class DummyLoggableCommand extends LoggableCommand
 {
-    protected static $defaultName = 'the:command:name';
+    private const DEFAULT_NAME = 'the:command:name';
+    protected static $defaultName = self::DEFAULT_NAME;
 }
